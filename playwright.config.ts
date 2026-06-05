e61879a84f795e4d9ade036182bd6b176e08cd05
Luka-Meladze-a1qa <l.meladze@a1qa.com>
2026-06-05 19:15:20 +0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,13 @@ export default defineConfig({
         testIdAttribute: "data-qa",
         navigationTimeout: 60_000,
         baseURL: process.env.BASE_URL || "https://www.automationexercise.com",
-        actionTimeout: 15_000
+        actionTimeout: 15_000,
+        launchOptions: {
+            args: [
+                '--disable-blink-features=AutomationControlled',
+                '--no-sandbox',
+            ],
+        },
     },
 
     projects: [
